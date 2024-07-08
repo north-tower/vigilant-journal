e1b13345535707fd892c209a2e0d9d916c447211
north-tower <muneshmyke@gmail.com>
2024-07-08 23:06:30 +0300

    Select Category

--- screens/JournalScreen.tsx
+++ screens/JournalScreen.tsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, Platform, KeyboardAvoidingView, Alert } from 'react-native';
+import {
+  View, Text, SafeAreaView, TouchableOpacity, ScrollView, Platform, KeyboardAvoidingView, Alert, StyleSheet,
+} from 'react-native';
 import tw from 'twrnc';
 import { Ionicons } from '@expo/vector-icons';
 import { TextInput, Button } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { RootStackParamList } from '../components/StackNavigator';
-import { Checkbox } from 'react-native-paper';
+import { Dropdown } from 'react-native-element-dropdown';
 
-export type NavigationProp = NativeStackNavigationProp<RootStackParamList, "Demo">;
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Demo'>;
 
-const JournalScreen = () => {
+
+interface Category {
+  label: string;
+  value: string;
+}
+
+
+const JournalScreen: React.FC = () => {
   const navigation = useNavigation<NavigationProp>();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [category, setCategory] = useState('');
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
-  const [checked, setChecked] = React.useState(false);
-  const [categories, setCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [value, setValue] = useState<string | null>(null);
 
   useEffect(() => {
     fetchCategories();
@@ -30,7 +39,8 @@ const JournalScreen = () => {
       const response = await fetch('https://postgres-js.vercel.app/getCategory');
       if (response.ok) {
         const data = await response.json();
-        setCategories(data.map((category: any) => category.category)); // Assuming category objects have a 'name' field
+        const formattedCategories = data.map((cat: any) => ({ label: cat.category, value: cat.category }));
+        setCategories(formattedCategories);
       } else {
         Alert.alert('Error', 'Failed to fetch categories');
       }
@@ -50,14 +60,14 @@ const JournalScreen = () => {
       const response = await fetch('https://postgres-js.vercel.app/journal', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           title,
           content,
-          category,
-          date: date.toISOString().split('T')[0] // format the date to 'YYYY-MM-DD'
-        })
+          category: value,
+          date: date.toISOString().split('T')[0], // format the date to 'YYYY-MM-DD'
+        }),
       });
       const data = await response.json();
       if (response.ok) {
@@ -67,7 +77,7 @@ const JournalScreen = () => {
         Alert.alert('Error', data.message || 'An error occurred');
       }
     } catch (error) {
-      Alert.alert('Error' || 'An error occurred');
+      Alert.alert('Error', 'An error occurred while saving the journal entry');
     }
   };
 
@@ -78,7 +88,7 @@ const JournalScreen = () => {
         <Text style={tw`text-white`}>Go Back</Text>
       </TouchableOpacity>
 
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={tw`flex-1`}>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={tw`flex-1`}>
         <ScrollView contentContainerStyle={tw`px-4 py-10`}>
           <View style={tw`mx-auto my-10 max-w-md rounded-xl bg-[#E5962D] px-4 py-10 shadow-lg sm:px-8`}>
             <Text style={tw`text-white text-2xl font-bold mb-6`}>New Journal Entry</Text>
@@ -104,16 +114,27 @@ const JournalScreen = () => {
                 underlineColor="white"
                 activeUnderlineColor="white"
               />
-              <TextInput
-                label="Category"
-                value={category}
-                onChangeText={setCategory}
-                style={tw`mb-4 bg-[#E5962D] text-white`}
-                underlineColor="white"
-                activeUnderlineColor="white"
+              <Dropdown
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={categories}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder="Select Category"
+                searchPlaceholder="Search..."
+                value={value}
+                onChange={item => {
+                  setValue(item.value);
+                }}
+                renderLeftIcon={() => (
+                  <Ionicons name="list" size={24} color="#E5962D" />
+                )}
               />
-               
-           
               <TouchableOpacity onPress={() => setShowDatePicker(true)} style={tw`mb-4 bg-[#E5962D]`}>
                 <TextInput
                   label="Date"
@@ -144,3 +165,29 @@ const JournalScreen = () => {
 };
 
 export default JournalScreen;
+
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.5,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
